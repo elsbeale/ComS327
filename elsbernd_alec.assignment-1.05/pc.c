99c2244019042c5b42f7fa45d5cdaf4c31b09f0a
RedBlueDevil <noahtang1997@yahoo.com>
2021-03-10 18:23:29 -0600

    added monster distance relative to pc

--- elsbernd_alec.assignment-1.05/pc.c
+++ elsbernd_alec.assignment-1.05/pc.c
@@ -172,9 +172,17 @@ uint32_t pc_next_pos(dungeon_t *d, pair_t dir){
       break;
       //downstairs or rest if not on stairs
     case 46:
+      if (d->map[dir[dim_y]][dir[dim_x]] == ter_stairs_down)
+      {
+        //generate a new dungeon, pc position, and monsters
+      }
       break;
       //upstairs
     case 44:
+      if (d->map[dir[dim_y]][dir[dim_x]] == ter_stairs_up)
+      {
+        //generate a new dungeon, pc position, and monsters
+      }
       break;
       //rest
     case 32:
@@ -182,6 +190,48 @@ uint32_t pc_next_pos(dungeon_t *d, pair_t dir){
       break;
       //m lists monsters
     case 109:
+      for (int i = 0; i < DUNGEON_Y; i++)
+      {
+        for (int j = 0; j < DUNGEON_X; j++)
+        { //STILL NEEDS WORK, not sure what to compare it to to check if there is a monster there
+          if (d->character[i][j]->alive) //need to check if there is a monster at the spot
+          {
+            int tmp_x = d->character[i][j]->position[dim_x] - d->pc.position[dim_x];
+            int tmp_y = d->character[i][j]->position[dim_y] - d->pc.position[dim_y];
+            if (tmp_x > 0)
+            {
+              //the monster is to the right of the pc. monster is east by tmp_x distance.
+
+            }
+            else if (tmp_x < 0)
+            {
+              //the monster is to the left of the pc. monster is west by tmp_x distance.
+              tmp_x = abs(tmp_x); //taking absolute value for printing purposes
+            }
+            else
+            {
+              //the monster is on the same column as the pc
+              //monster is 0 distance east of the pc
+            }
+            if (tmp_y > 0)
+            {
+              //the monster is below the pc. monster is south by tmp_y distance
+            }
+            else if (tmp_y < 0)
+            {
+              //monster is above the pc. monster is north by tmp_y distance
+              tmp_y = abs(tmp_y); //taking absolute value for printing purposes
+            }
+            else
+            {
+              //the monster is on the same row as the pc
+              //monster is 0 distance north of the pc
+            }
+
+          }
+
+        }
+      }
       break;
       // scroll up list if list is up
     case 259:
