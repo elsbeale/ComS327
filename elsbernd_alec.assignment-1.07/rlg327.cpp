12400f22e1b59a3ee001735071e967e73ca176a0
RedBlueDevil <noahtang1997@yahoo.com>
2021-04-01 12:06:06 -0500

    fix

--- elsbernd_alec.assignment-1.07/rlg327.cpp
+++ elsbernd_alec.assignment-1.07/rlg327.cpp
@@ -418,7 +418,7 @@ int main(int argc, char *argv[])
 
           for (int i = 0; i < char_count - 2; i++)
           {
-            printf("%c", temp_int_arr[i]);
+            std::cout << temp_int_arr[i] << std::endl;
           }
           // std::string stbase;
           // std::string stdice;
@@ -437,7 +437,7 @@ int main(int argc, char *argv[])
           //   {
           //     stsides.append(std::to_string(temp_int_arr[i]));
           //   }
-          }
+          // }
 
 
           // bool plus_trigger = false;
@@ -468,9 +468,9 @@ int main(int argc, char *argv[])
           //     stsides.append(std::to_string(temp_arr[i]));
           //   }
           // }
-          temp.sbase = stoi(stbase);
-          temp.sdice = stoi(stdice);
-          temp.ssides = stoi(stsides);
+          // temp.sbase = stoi(stbase);
+          // temp.sdice = stoi(stdice);
+          // temp.ssides = stoi(stsides);
           
         }
         else if (word == "DAM")
